1a1a2527d9ddd3a25969aa60e28081c3b6edf413
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-04 07:40:05 +0100

    Added offline.html to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,24 +22,25 @@ const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
+    '/offline.html',
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
+    console.log('[ServiceWorker] Install');
+    // CODELAB: Precache static resources here.
 
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
+    console.log('[ServiceWorker] Activate');
+    // CODELAB: Remove previous cached data from disk.
 
-  self.clients.claim();
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    // CODELAB: Add fetch event handler here.
 
 });
