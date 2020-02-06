703d291b92074523807574a768dea9c6652b7043
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-06 07:25:44 +0100

    updating files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,7 +23,28 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-    '/offline.html',
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/scripts/install.js',
+    '/scripts/luxon-1.11.4.js',
+    '/styles/inline.css',
+    '/images/add.svg',
+    '/images/clear-day.svg',
+    '/images/clear-night.svg',
+    '/images/cloudy.svg',
+    '/images/fog.svg',
+    '/images/hail.svg',
+    '/images/install.svg',
+    '/images/partly-cloudy-day.svg',
+    '/images/partly-cloudy-night.svg',
+    '/images/rain.svg',
+    '/images/refresh.svg',
+    '/images/sleet.svg',
+    '/images/snow.svg',
+    '/images/thunderstorm.svg',
+    '/images/tornado.svg',
+    '/images/wind.svg',
 ];
 
 self.addEventListener('install', (evt) => {
