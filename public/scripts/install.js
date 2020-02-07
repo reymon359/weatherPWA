2876c372da841fac62e1b68e39290b780b657d32
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-07 07:41:42 +0100

    Log the user choice

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -48,6 +48,15 @@ function installPWA(evt) {
     // Hide the install button, it can't be called twice.
     evt.srcElement.setAttribute('hidden', true);
     // CODELAB: Log user response to prompt.
+    deferredInstallPrompt.userChoice
+        .then((choice) => {
+            if (choice.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt', choice);
+            } else {
+                console.log('User dismissed the A2HS prompt', choice);
+            }
+            deferredInstallPrompt = null;
+        });
 
 }
 
